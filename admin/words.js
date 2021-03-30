045ec197f7c9520192cf4e4e6b08d9afdccbbfd4
Holger Stöcker <git@hst-it.de>
2021-03-30 20:48:28 +0200

    initial webrequest with axois and some small changes

--- admin/words.js
+++ admin/words.js
@@ -5,37 +5,17 @@ systemDictionary = {
 	"proplanta adapter settings": {
 		"en": "Adapter settings for proplanta",
 		"de": "Adaptereinstellungen für proplanta",
-		"ru": "Настройки адаптера для proplanta",
-		"pt": "Configurações do adaptador para proplanta",
-		"nl": "Adapterinstellingen voor proplanta",
-		"fr": "Paramètres d'adaptateur pour proplanta",
-		"it": "Impostazioni dell'adattatore per proplanta",
-		"es": "Ajustes del adaptador para proplanta",
-		"pl": "Ustawienia adaptera dla proplanta",
-		"zh-cn": "proplanta的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+	"city": {
+		"en": "city",
+		"de": "Stadt/Ort",
 	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+	"countycode": {
+		"en": "countrycode (optional)",
+		"de": "Ländercode (optional)",
+	},
+	"instructions": {
+		"en": "Navigate to <a href='https://www.proplanta.de/Agrar-Wetter/Deutschland/' target='_blank'>https://www.proplanta.de/Agrar-Wetter/Deutschland/</a> and search for a city. The city must be selectable in the dropdown. Pay attention to the capital letters in the city names. Countycode is optional. Possible values: de (default), at, ch, fr, it",
+		"de": "Auf Webseite <a href='https://www.proplanta.de/Agrar-Wetter/Deutschland/' target='_blank'>https://www.proplanta.de/Agrar-Wetter/Deutschland/</a> nach gewünschtem Ort suchen. Der Ort muss über das Dropdown auswählbar sein. Auf die Großbuchstaben im Ortnamen achten. Der Ländercode ist optional. Mögliche Werte hierfür: de (default), at, ch, fr, it",
 	}
 };
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//based on: https://svn.fhem.de/fhem/trunk/fhem/FHEM/59_PROPLANTA.pm
+//Hypnos3:	https://github.com/ioBroker/AdapterRequests/issues/87
+
 "use strict";
 
 /*
@@ -7,6 +10,8 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
+const axios = require('axios');
+const adapterName = require('./package.json').name.split('.').pop();
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
@@ -36,53 +41,125 @@ class Proplanta extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.log.info("config city: " + this.config.city);
+		this.log.info("config countrycode: " + this.config.countrycode);
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+		// Change Umlaute from UTF8 in Percent-encode, also space to plus symbol
+		const cityUrlEncoded = this.config.city.replace("Ä", "%C4").replace("Ö", "%D6").replace("Ü", "%DC").replace("ß", "%DF").replace("ä", "%E4").replace("ö", "%F6").replace("ü", "%FC").replace(" ", "+");
+		this.log.info("cityUrlEncoded: " + cityUrlEncoded);
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+		// Generate URL variables
+		const baseUrl = "https://www.proplanta.de/";
+		const requestString = "";
+		if (this.config.countrycode == "de" || this.config.countrycode == "") {
+			requestString = "/Wetter/profi-wetter.php?SITEID=60&PLZ=" + cityUrlEncoded + "&STADT=" + cityUrlEncoded + "&WETTERaufrufen=stadt&Wtp=&SUCHE=Wetter&wT=";
+		}
+		else if (this.config.countrycode == "at") {
+			requestString = "/Wetter-Oesterreich/profi-wetter-at.php?SITEID=60&PLZ=" + cityUrlEncoded + "&STADT=" + cityUrlEncoded + "&WETTERaufrufen=stadt&Wtp=&SUCHE=Wetter-Oesterreich&wT=";
+		}
+		else if (this.config.countrycode == "ch") {
+			requestString = "/Wetter-Schweiz/profi-wetter-ch.php?SITEID=60&PLZ=" + cityUrlEncoded + "&STADT=" + cityUrlEncoded + "&WETTERaufrufen=stadt&Wtp=&SUCHE=Wetter-Schweiz&wT=";
+		}
+		else if (this.config.countrycode == "fr") {
+			requestString = "/Wetter-Frankreich/profi-wetter-fr.php?SITEID=60&PLZ=" + cityUrlEncoded + "&STADT=" + cityUrlEncoded + "&WETTERaufrufen=stadt&Wtp=&SUCHE=Wetter-Frankreich&wT=";
+		}
+		else if (this.config.countrycode == "it") {
+			requestString = "/Wetter-Italien/profi-wetter-it.php?SITEID=60&PLZ=" + cityUrlEncoded + "&STADT=" + cityUrlEncoded + "&WETTERaufrufen=stadt&Wtp=&SUCHE=Wetter-Italien&wT=";
+		}
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+		// Start webrequest
+		axios({
+			method: 'get',
+			baseURL: baseUrl,
+			url: requestString,
+			timeout: 10000,
+			responseType: 'document'
+		}).then(
+			async (response) => {
+				const content = response.data;
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+				this.log.info('webrequest done');
+				this.log.info('received data (' + response.status + '): ' + content);
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+				/*await this.setObjectNotExistsAsync(path + 'responseCode', {
+					type: 'state',
+					common: {
+						name: 'responseCode',
+						type: 'number',
+						role: 'value',
+						read: true,
+						write: false
+					},
+					native: {}
+				});
+				this.setState(path + 'responseCode', {val: response.status, ack: true});
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+				if (content && Object.prototype.hasOwnProperty.call(content, 'sensordatavalues')) {
+					for (const key in content.sensordatavalues) {
+						const obj = content.sensordatavalues[key];
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+						let unit = null;
+						let role = 'value';
+
+						if (obj.value_type.indexOf('SDS_') == 0) {
+							unit = 'µg/m³';
+							role = 'value.ppm';
+						} else if (obj.value_type.indexOf('temperature') >= 0) {
+							unit = '°C';
+							role = 'value.temperature';
+						} else if (obj.value_type.indexOf('humidity') >= 0) {
+							unit = '%';
+							role = 'value.humidity';
+						} else if (obj.value_type.indexOf('pressure') >= 0) {
+							unit = 'Pa';
+							role = 'value.pressure';
+						} else if (obj.value_type.indexOf('noise') >= 0) {
+							unit = 'dB(A)';
+							role = 'value';
+						} else if (Object.prototype.hasOwnProperty.call(unitList, obj.value_type)) {
+							unit = unitList[obj.value_type];
+							role = roleList[obj.value_type];
+						}
+
+						await this.setObjectNotExistsAsync(path + obj.value_type, {
+							type: 'state',
+							common: {
+								name: obj.value_type,
+								type: 'number',
+								role: role,
+								unit: unit,
+								read: true,
+								write: false
+							},
+							native: {}
+						});
+						this.setState(path + obj.value_type, {val: parseFloat(obj.value), ack: true});
+					}
+				} else {
+					this.log.warn('Response has no valid content. Check hostname/IP address and try again.');
+				}*/
+
+			}
+		).catch(
+			(error) => {
+				if (error.response) {
+					// The request was made and the server responded with a status code
+
+					this.log.warn('received error ' + error.response.status + ' response from local sensor ' + sensorIdentifier + ' with content: ' + JSON.stringify(error.response.data));
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js<div></div>
+					this.log.error(error.message);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					this.log.error(error.message);
+				}
+			}
+		);
+
+		// Stop adapter because this is a cronjob and we need to kill the instance when everything is done.
+		this.killTimeout = setTimeout(this.stop.bind(this), 15000);
 	}
 
 	/**
@@ -103,23 +180,6 @@ class Proplanta extends utils.Adapter {
 		}
 	}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
 	/**
 	 * Is called if a subscribed state changes
 	 * @param {string} id
@@ -134,25 +194,6 @@ class Proplanta extends utils.Adapter {
 			this.log.info(`state ${id} deleted`);
 		}
 	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
 }
 
 if (require.main !== module) {
